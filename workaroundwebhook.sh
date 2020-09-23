28c082b037bd4e33918abab67daca949d801acd9
Yanbing Wang <yawang@microsoft.com>
2020-09-22 22:09:27 -0700

    add workaround

---
+++ workaroundwebhook.sh
@@ -0,0 +1,6 @@
+kubectl delete -f https://raw.githubusercontent.com/kubernetes-sigs/windows-testing/master/helpers/hyper-v-mutating-webhook/deployment.yaml
+cat e2e-builder.template.yaml | envsubst | kubectl create -f -
+curl -OL https://raw.githubusercontent.com/kubernetes-sigs/windows-testing/master/extensions/hyperv-mutating-webhook/v1/configure-hyperv-webhook.sh
+chmod +x configure-hyperv-webhook.sh
+./configure-hyperv-webhook.sh
+rm configure-hyperv-webhook.sh
