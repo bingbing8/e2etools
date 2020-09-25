7e1ab07f95ab0572249091688f77982a4b0f2fa6
Yanbing Wang <yawang@microsoft.com>
2020-09-24 22:57:06 -0700

    add sleep

--- workaroundwebhook.sh
+++ workaroundwebhook.sh
@@ -1,5 +1,6 @@
 kubectl delete -f https://raw.githubusercontent.com/kubernetes-sigs/windows-testing/master/helpers/hyper-v-mutating-webhook/deployment.yaml
 cat e2e-builder.template.yaml | envsubst | kubectl create -f -
+sleep 2m
 kubectl logs -f e2e-builder
 curl -OL https://raw.githubusercontent.com/kubernetes-sigs/windows-testing/master/extensions/hyperv-mutating-webhook/v1/configure-hyperv-webhook.sh
 chmod +x configure-hyperv-webhook.sh
