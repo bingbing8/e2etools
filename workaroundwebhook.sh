222c267ba6e5caa66ae429c31616eb256101ece9
Yanbing Wang <yawang@microsoft.com>
2020-09-25 17:18:48 -0700

    add check for e2e-builder

--- workaroundwebhook.sh
+++ workaroundwebhook.sh
@@ -1,4 +1,5 @@
 kubectl delete -f https://raw.githubusercontent.com/kubernetes-sigs/windows-testing/master/helpers/hyper-v-mutating-webhook/deployment.yaml
+kubectl get pod e2e-builder && kubectl delete pod e2e-builder || echo "e2e-builder does not exist"
 cat e2e-builder.template.yaml | envsubst | kubectl create -f -
 sleep 2m
 kubectl logs -f e2e-builder
