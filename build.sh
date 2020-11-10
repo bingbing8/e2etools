216bb168c948b9903affbe7890004c1f72824099
Yanbing Wang <yawang@microsoft.com>
2020-11-09 17:06:02 -0800

    get code from kubernetes

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -o nounset                              # Treat unset variables as an error
 
 apt-get update && apt-get install rsync -y
 
-K8S_REPO=http://github.com/bingbing8/kubernetes
+K8S_REPO=http://github.com/kubernetes/kubernetes
 K8S_SRC_PATH=${K8S_SRC_PATH:-/go/src/k8s.io/kubernetes}
 
 trap "rm -f ${K8S_SRC_PATH}" ERR 
