9bde1a4e7f4cc42088d2d9198ca22b5304122257
Yanbing Wang <yawang@microsoft.com>
2021-01-20 10:19:52 -0800

    check environment variable

--- build.sh
+++ build.sh
@@ -8,13 +8,19 @@ apt-get update && apt-get install rsync -y
 
 K8S_REPO=http://github.com/kubernetes/kubernetes
 K8S_SRC_PATH=${K8S_SRC_PATH:-/go/src/k8s.io/kubernetes}
-K8S_BRANCH=${K8S_BRANCH:master}
 
 
 trap "rm -f ${K8S_SRC_PATH}" ERR 
 
 rm -rf ${K8S_SRC_PATH}
-git clone -b ${K8S_BRANCH} ${K8S_REPO} ${K8S_SRC_PATH}
+
+if [ -z "$K8S_BRANCH" ]
+then
+      git clone ${K8S_REPO} ${K8S_SRC_PATH}
+else
+      git clone -b ${K8S_BRANCH} ${K8S_REPO} ${K8S_SRC_PATH}
+
+
 
 pushd ${K8S_SRC_PATH}
 
