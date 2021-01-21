b08d322582748fcef21b2dab1d135e9bf50bcb41
Yanbing Wang <yawang@microsoft.com>
2021-01-20 19:19:46 -0800

    update version

--- startbuild.sh
+++ startbuild.sh
@@ -1,7 +1,7 @@
 curl https://k8swin.blob.core.windows.net/k8s-windows/kubetest -o $K8S_ROOT/kubetest
 chmod +x $K8S_ROOT/kubetest
 KUBE_TEST_REPO_LIST=/tmp/repo_list
-K8S_BRANCH="release-1.20"
+K8S_BRANCH="release-1.19"
 curl https://raw.githubusercontent.com/kubernetes-sigs/windows-testing/master/images/image-repo-list -o $KUBE_TEST_REPO_LIST
 kubectl get pod e2e-builder && kubectl delete pod e2e-builder || echo "e2e-builder does not exist"
 cat e2e-builder.template.yaml | envsubst | kubectl create -f -
